91651c562399c2ff8d6e5d3899c2a7b9d1b1d049
Gadzhiev <gadzhiev.farid@gmail.com>
2017-07-20 12:37:00 +0300

    Syncronized and volatile task

---
+++ src/main/java/task_06/Rabbit.java
@@ -0,0 +1,34 @@
+package main.java.task_06;
+
+import java.io.IOException;
+
+public class Rabbit extends Thread {
+    public static volatile boolean flag = true;
+
+    public static void main(String[] args) {
+        new stopEating().start();
+        new eating().start();
+    }
+
+    public static class stopEating extends Thread {
+        @Override
+        public void run() {
+            try {
+                int stop = System.in.read();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            flag = false;
+            System.err.println("Rabbit is full");
+        }
+    }
+
+    public static class eating extends Thread {
+        @Override
+        public void run() {
+            while (flag) {
+                System.err.println("eating...");
+            }
+        }
+    }
+}
